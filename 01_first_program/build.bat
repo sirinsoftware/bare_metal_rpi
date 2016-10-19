0e42ee56c7a3791ffb352dd7444793e86a0ef29f
DimaWittmann <dimawittmann@gmail.com>
2016-10-19 11:14:15 +0300

    add bat files.

---
+++ 01_first_program/build.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+arm-none-eabi-gcc -O0 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g main.c -o kernel.elf
+arm-none-eabi-objcopy kernel.elf -O binary kernel.img
+arm-none-eabi-objdump -D kernel.elf > kernel.dump
+
